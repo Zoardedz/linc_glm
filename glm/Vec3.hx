b7e09ed9f7480444c0186e36bfc8646e1a7783ac
Ziad <zizozoza1273@gmail.com>
2024-04-12 21:24:49 +0200

    Revert "added set callbacks for camera movement efficiency"
    This reverts commit 729ef3ff816e86b812be806cc392e18dc2e0a7cd.

--- glm/Vec3.hx
+++ glm/Vec3.hx
@@ -3,62 +3,33 @@ package glm;
 import cpp.Float32 as F32;
 import cpp.Pointer;
 import glm.GLM;
-import cpp.Star;
 
-@:forward
-abstract Vec3(Star<NativeVec3>) to Star<NativeVec3> from Star<NativeVec3>
-{
-	//! gotta do this for callback
-	public var x(get, set):F32;
-	public function get_x():F32 return this.xx;
-	public function set_x(n:F32):F32 {
-		this.xx = n;
-		untyped __cpp__('if ((*(this1)).xSetCallback == nullptr) return n;');
-		this.xSet();
-		return n;
-	}
-	
-	public var y(get, set):F32;
-	public function get_y():F32 return this.yy;
-	public function set_y(n:F32):F32 {
-		this.yy = n;
-		untyped __cpp__('if ((*(this1)).ySetCallback == nullptr) return n;');
-		this.ySet();
-		return n;
-	}
-	
-	public var z(get, set):F32;
-	public function get_z():F32 return this.zz;
-	public function set_z(n:F32):F32 {
-		this.zz = n;
-		untyped __cpp__('if ((*(this1)).zSetCallback == nullptr) return n;');
-		this.zSet();
-		return n;
-	}
-	
-	public function new(x:F32 = 0.0, y:F32 = 0.0, z:F32 = 0.0, ?underlying:cpp.Pointer<NativeVec3> = null)
-		this = underlying != null ? underlying.ptr : NativeVec3.heapInit(x, y, z);
-		
-	public inline function syncVec3(otherNative:Pointer<NativeVec3>):Void
-	{
-		final vec3:Vec3 = otherNative.ptr;	
-		x = vec3.x;
-		y = vec3.y;
-		z = vec3.z;
-	}
-	
-	public inline function toString():String
-		return 'x: ${x}, y: ${y}, z: ${z}';
-		
-	@:op(A - B)
-	public function subtract(rhs:Vec3):NativeVec3
-	{
-		return untyped __cpp__('(*{0}) - (*{1})', this, rhs);
-	}
-	
-	@:to
-	public inline function to():NativeVec3
-		return this;
+class Vec3 {
+    public var underlying:NativeVec3;
+
+    public var x(get, set):F32;
+    public function get_x():F32 return underlying.x;
+    public function set_x(n:F32):F32 return underlying.x = n;
+
+    public var y(get, set):F32;
+    public function get_y():F32 return underlying.y;
+    public function set_y(n:F32):F32 return underlying.y = n;
+
+    public var z(get, set):F32;
+    public function get_z():F32 return underlying.z;
+    public function set_z(n:F32):F32 return underlying.z = n;
+
+    public function new(x:F32 = 0.0, y:F32 = 0.0, z:F32 = 0.0, ?underlying:cpp.Pointer<NativeVec3> = null)
+        underlying != null ? this.underlying = untyped __cpp__('*{0}', underlying.ptr) : this.underlying = NativeVec3.vec3Init(x, y, z);
+
+    public inline function syncVec3(otherNative:Pointer<NativeVec3>):Void {
+        this.x = otherNative.ptr.x;
+        this.y = otherNative.ptr.y;
+        this.z = otherNative.ptr.z;
+    }
+    
+    public inline function toString():String 
+        return 'x: $x, y: $y, z: $z';
 }
 
 @:unreflective
